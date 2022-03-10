2168bfc655011e241290d53b1c8a877bfce3c75e
adamjmcgrath <adam.mcgrath@auth0.com>
2022-03-10 11:46:42 +0000

    test edoic

--- test/example.e2e.js
+++ test/example.e2e.js
@@ -1,73 +1,53 @@
-const http = require('http');
 const payloads = require('../payloads');
-
-function old_isSafeRedirect(url) {
-    if (typeof url !== 'string') {
-        throw new TypeError(`Invalid url: ${url}`);
-    }
-
-    // Prevent open redirects using the //foo.com format (double forward slash).
-    if (/\/\//.test(url)) {
-        return false;
-    }
-
-    return !/^[a-zA-Z][a-zA-Z\d+\-.]*:/.test(url);
-}
-
-
-const safeBaseUrl = new URL('http://localhost:3000');
-function toSafeRedirect(dangerousRedirect) {
-    
-    let url;
-    try {
-        url = new URL(dangerousRedirect, safeBaseUrl);
-    } catch (e) {
-        return undefined;
-    }
-    if (url.origin === safeBaseUrl.origin) {
-        return url.toString();
-    }
-    return undefined;
-}
+const express = require('express');
 
 describe('open redirect', () => {
 
     let server;
     let ok;
     let bad;
-    
+    let redirected;
+
+    beforeEach(() => {
+        redirected = false;
+    });
+
     before((done) => {
         ok = 0;
         bad = 0;
-        server = http.createServer((req, res) => {
-            const q = new URL(req.url, 'http://localhost:3000').searchParams;
-            try {
-                const url = toSafeRedirect(q.get('url'));
-                if (q.get('url') && url) {
-                    res.writeHead(302, {
-                        Location: url
-                    });    
-                }
-            } catch(e) {
-                // Do nothing, node can't write the header
+        const app = express();
+
+
+        app.use((req, res, next) => {
+            if (redirected) {
+                next()
+            } else {
+                // Collapse leading slashes fix:
+                const url = req.originalUrl.replace(/^\/+/, '/');
+                res.redirect(url);
+                redirected = true;
             }
-            
-            res.end();
+
         });
-        server.listen(3000, done);
+
+        app.get('/', (req, res) => {
+            res.send(`hello`);
+        });
+        server = app.listen(3000, done);
     });
-    
+
     after((done) => {
         console.log('OK', ok, ',', 'BAD', bad);
         server.close(done);
     });
-    
+
     for (let payload of payloads) {
         it(`should not visit external host with ${payload}`, async () => {
             await browser.setTimeout({ 'pageLoad': 1000 });
-            await browser.url(`/?url=${payload}`);
+            const url = `http://localhost:3000${ payload.startsWith('/') ? '' : '/' }${payload}`;
+            await browser.url(url);
             const hostname = new URL(await browser.getUrl()).hostname
-            
+
             const isOk = hostname === 'localhost';
             if (isOk) {
                 ok++;
